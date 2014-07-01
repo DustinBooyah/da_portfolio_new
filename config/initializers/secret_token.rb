2a84635958c7f99fdd3bbaa36dcd05517e90c77a
Dustin Aguilar <dustin@dustinaguilar.com>
2014-07-01 06:40:56 -0400

    First Commit
    New rails application that will be my portfolio website

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DaPortfolio::Application.config.secret_key_base = '6a8ba6ad72bb076898f3b51bcedb89fbb2e57bf94782133d78a98d0002377b25ff5910145573b72b6db464887db73cc5014cf91e210cb1038055d883c414179a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DaPortfolio::Application.config.session_store :cookie_store, key: '_da_portfolio_session'
