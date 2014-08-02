0bbe7b74726886da4484813f35c2283662bf9269
Dustin Aguilar <dustin@dustinaguilar.com>
2014-08-02 10:21:03 -0400

    Basic Chassis Finished
    Installed Foundation, made a few tweaks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require foundation
+$(document).foundation();
 //= require_tree .
+
+$(function(){ $(document).foundation(); });
